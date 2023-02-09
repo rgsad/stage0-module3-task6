468cad6adc6e91e406241f7554c382974b7b327f
rgsad <rsad3262@gmail.com>
2023-02-09 22:19:33 +0300

    start task

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int number = Math.round(numberToBeRounded);
+        System.out.println(number);
     }
 }
